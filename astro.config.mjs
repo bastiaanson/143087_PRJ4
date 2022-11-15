aa2d99883ac382c3403910046d7d5fe9e42d5d2d
bastiaanson <143087@buas.nl>
2022-11-15 15:37:43 +0100

    github pages push check
    Signed-off-by: bastiaanson <143087@buas.nl>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://bastiaanson.github.io',
+    base: '/143087_PRJ4',
+});
